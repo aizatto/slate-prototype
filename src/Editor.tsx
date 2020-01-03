7fb8ce8a36e5a6f46a899ec09f373cd739064724
Aizat Faiz <aizat.faiz@gmail.com>
2020-01-03 12:36:43 +0800

    upgrade slate from 0.47.4 to 0.57.1

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,74 +1,85 @@
-import React, {useState} from 'react';
-const Code = require("@convertkit/slate-code").default;
+import React, {useState, useMemo} from 'react';
 
-const {Editor: SlateEditor} = require('slate-react');
-const { Value } = require('slate');
-const MDserializer = require('slate-md-serializer').default;
+// Import the Slate editor factory.
+import { createEditor, Node } from 'slate'
 
-const plugins = [Code({
-  highlight: true,
-  block: "code",
-  line: "code-line",
-  classNames: {
-    block: "code",
-    line: "code-line"
-  }
-})]
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            text: 'A line of text in a paragraph.',
-          },
-        ],
-      },
-    ],
-  },
-  annotations: {},
-  decorations: {},
-});
+const MDserializerLib = require('slate-md-serializer').default;
+const MDserializer = new MDserializerLib();
 
-function handleKeyDown(event: any, editor: any, next: any) {
-  if (event.ctrlKey) {
-    switch (event.key) {
-      case 'y':
-        event.preventDefault();
-        console.log('here');
-        editor.insertCode({ code: '<h1>Heading 1</h1>', language: "html" });
-        break;
-      default:
-        return next();
-    }
-  } else {
-    return next()
-  }
-}
+// const plugins = [Code({
+//   highlight: true,
+//   block: "code",
+//   line: "code-line",
+//   classNames: {
+//     block: "code",
+//     line: "code-line"
+//   }
+// })]
+
+// const initialValue = Value.fromJSON({
+//   document: {
+//     nodes: [
+//       {
+//         object: 'block',
+//         type: 'paragraph',
+//         nodes: [
+//           {
+//             object: 'text',
+//             text: 'A line of text in a paragraph.',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   annotations: {},
+//   decorations: {},
+// });
+
+// function handleKeyDown(event: any, editor: any, next: any) {
+//   if (event.ctrlKey) {
+//     switch (event.key) {
+//       case 'y':
+//         event.preventDefault();
+//         console.log('here');
+//         editor.insertCode({ code: '<h1>Heading 1</h1>', language: "html" });
+//         break;
+//       default:
+//         return next();
+//     }
+//   } else {
+//     return next()
+//   }
+// }
+
 
 const Editor: React.FC<any> = () => {
-  const [value, setValue] = useState(initialValue);
-  
-  console.log(value);
-  console.log(value.toJSON());
-  /*
-  const md = new MDserializer();
+  const editor = useMemo(() => withReact(createEditor()), [])
 
-
-  console.log(new MDserializer().serialize(value));
-  */
+  // Keep track of state for the value of the editor.
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
 
   return (
-    <SlateEditor
-      plugins={plugins}
+    <Slate
+      editor={editor}
       value={value}
-      onChange={({ value: newValue }: any) => setValue(newValue)}
-      onKeyDown={handleKeyDown}
-    />
+      onChange={value => {
+        console.log(value);
+        setValue(value);
+        // const markdown = MDserializer.serialize({ document: { nodes: value }});
+        // console.log(markdown);
+        // const markdown = value.map(node => MDserializer.renderNode(node));
+        // console.log(markdown.join("\n"));
+      }}>
+      <Editable />
+    </Slate>
   );
 }
 
